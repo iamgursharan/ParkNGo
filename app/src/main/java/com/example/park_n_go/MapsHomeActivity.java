5db65ea35fd85629d2d40e44c56be84674fdbac3
iamgu <iamgu@ACER>
2019-04-07 17:36:05 -0400

    0.1.4 Added maps_style metho

--- app/src/main/java/com/example/park_n_go/MapsHomeActivity.java
+++ app/src/main/java/com/example/park_n_go/MapsHomeActivity.java
@@ -7,33 +7,48 @@ package com.example.park_n_go;
 
 
 import android.Manifest;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
@@ -50,6 +65,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Logger;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.annotations.NotNull;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -59,19 +75,39 @@ import java.util.List;
 import java.util.ListIterator;
 import java.util.Locale;
 
-public class MapsHomeActivity extends  FragmentActivity implements OnMapReadyCallback{
+
+public class MapsHomeActivity extends  AppCompatActivity implements OnMapReadyCallback,LocationListener, GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener{
 
     // field variables
     private GoogleMap mMap;
     private static FirebaseDatabase database;
     private static DatabaseReference databaseRef;
+
+    /**
+     * Request code for location permission request.
+     *
+     * @see #onRequestPermissionsResult(int, String[], int[])
+     */
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+
+    /**
+     * Flag indicating whether a requested permission has been denied after returning in
+     * {@link #onRequestPermissionsResult(int, String[], int[])}.
+     */
+    private boolean mPermissionDenied = false;
+
     private static final int ZOOM_LEVEL = 12;
     private static final int TILT_LEVEL = 1;
-    private static final int BEARING_LEVEL = 50;
+    private static final int BEARING_LEVEL = 1;
     private static final int MY_LOCATION_REQUEST_CODE = 400;
     private String searchAddress;
     private static String TAG="TAG";
+    private Button searchBtn;
     private GoogleApiClient mGoogleApiClient;
+    private LocationManager locationManager;
+    private Double shareLatitude;
+    private Double shareLongitude;
 
 
 
@@ -80,11 +116,16 @@ public class MapsHomeActivity extends  FragmentActivity implements OnMapReadyCal
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps_home);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+        final SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        EditText searchET=(EditText)findViewById(R.id.search_et);
-        searchAddress=searchET.getText().toString();
+        searchBtn=(Button) findViewById(R.id.search_button);
+        // Calling styleSearchButton
+        styleSearchBtn();
+        enableMyLocation();
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, (LocationListener) this);
+
    }
 
 
@@ -101,6 +142,7 @@ public class MapsHomeActivity extends  FragmentActivity implements OnMapReadyCal
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.setMyLocationEnabled(true);
         checkLocationPermission(mMap);
         database = FirebaseDatabase.getInstance();
         databaseRef=database.getReference();
@@ -121,12 +163,52 @@ public class MapsHomeActivity extends  FragmentActivity implements OnMapReadyCal
 
 
         });
+//        This functionality is currently deprecated
+        /*
+        final ToggleButton mapsStyleToggleBtn=(ToggleButton)findViewById(R.id.maps_style_toggle);
+        mapsStyleToggleBtn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    // For loading customized styles for map
+                    try {
+                        boolean success = mMap.setMapStyle(
+                                MapStyleOptions.loadRawResourceStyle(
+                                        getApplicationContext(), R.raw.styles_map_night));
 
-     // For loading customized styles for map
+                        if (!success) {
+                            Toast toast = Toast.makeText(getApplicationContext(), "Unable to load style file", Toast.LENGTH_LONG);
+                            toast.show();
+                        }
+                    } catch (Resources.NotFoundException e) {
+                        Toast toast = Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG);
+                        toast.show();
+                    }
+                    Drawable whiteCircle= ResourcesCompat.getDrawable(getApplicationContext().getResources(),R.drawable.circle_white_with_image,null);
+                    mapsStyleToggleBtn.setBackgroundDrawable(whiteCircle);
+                }
+                else{
+                    try {
+                        boolean success = mMap.setMapStyle(
+                                MapStyleOptions.loadRawResourceStyle(
+                                        getApplicationContext(), R.raw.styles_map_standard));
+
+                        if (!success) {
+                            Toast toast = Toast.makeText(getApplicationContext(), "Unable to load style file", Toast.LENGTH_LONG);
+                            toast.show();
+                        }
+                    } catch (Resources.NotFoundException e) {
+                        Toast toast = Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG);
+                        toast.show();
+                    }
+                }
+            }
+        });*/
+        // For loading customized styles for map
         try {
             boolean success = mMap.setMapStyle(
                     MapStyleOptions.loadRawResourceStyle(
-                            this, R.raw.styles_map_standard));
+                            getApplicationContext(), R.raw.styles_map_standard));
 
             if (!success) {
                 Toast toast = Toast.makeText(getApplicationContext(), "Unable to load style file", Toast.LENGTH_LONG);
@@ -138,15 +220,21 @@ public class MapsHomeActivity extends  FragmentActivity implements OnMapReadyCal
         }
 
 
+
     }
 
-    private void fillMapWithMarkers(List<Parking> parkings) throws IOException {
+    private void fillMapWithMarkers(final List<Parking> parkings) throws IOException {
        int count=0;
-
-       search(parkings,searchAddress);
+       searchBtn.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+                  search(parkings);
+         }
+      });
 
        if(mMap==null)
             return;
+
         for(Parking parking:parkings)
          { count++;
              final LatLng position = new LatLng(Double.parseDouble(parking.getLatitude()),
@@ -163,7 +251,7 @@ public class MapsHomeActivity extends  FragmentActivity implements OnMapReadyCal
         }
     }
 
-    private List<Parking> readData(DataSnapshot dataSnapshot)
+    private List<Parking> readData(@NonNull DataSnapshot dataSnapshot)
     {
         List<Parking> parkings=new ArrayList<>();
         for(DataSnapshot snapshot: dataSnapshot.getChildren())
@@ -180,16 +268,27 @@ public class MapsHomeActivity extends  FragmentActivity implements OnMapReadyCal
         super.onStart();
     }
 
+    /**
+     * Enables the My Location layer if the fine location permission has been granted.
+     */
+    private void enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission to access the location is missing.
+            PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
+                    Manifest.permission.ACCESS_FINE_LOCATION, true);
+        } else if (mMap != null) {
+            // Access to the location has been granted to the app.
+            mMap.setMyLocationEnabled(true);
+        }
+    }
+
     //Location permission region starts
     public void checkLocationPermission(GoogleMap mMap) {
 
     if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
             == PackageManager.PERMISSION_GRANTED) {
         mMap.setMyLocationEnabled(true);
-
-
-        Toast toast = Toast.makeText(getApplicationContext(), "Latitude:longitude:", Toast.LENGTH_LONG);
-        toast.show();
     } else {
         // Show rationale and request permission.
         Toast toast = Toast.makeText(getApplicationContext(), "Enter address in the search text box", Toast.LENGTH_LONG);
@@ -197,44 +296,148 @@ public class MapsHomeActivity extends  FragmentActivity implements OnMapReadyCal
         // Set search text box to focus.
     }
 
+
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-
-
-        if (requestCode == MY_LOCATION_REQUEST_CODE) {
-            if (permissions.length == 1 &&
-                    permissions[0] == Manifest.permission.ACCESS_FINE_LOCATION &&
-                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Toast toast = Toast.makeText(getApplicationContext(), "Location shared", Toast.LENGTH_LONG);
-                toast.show();
-            } else {
-                Toast toast = Toast.makeText(getApplicationContext(), "Enter address in the search text box", Toast.LENGTH_LONG);
-                toast.show();
-            }
-        }
-    }
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+//
+//
+//        if (requestCode == MY_LOCATION_REQUEST_CODE) {
+//            if (permissions.length == 1 &&
+//                    permissions[0] == Manifest.permission.ACCESS_FINE_LOCATION &&
+//                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                Toast toast = Toast.makeText(getApplicationContext(), "Location shared", Toast.LENGTH_LONG);
+//                toast.show();
+//            } else {
+//                Toast toast = Toast.makeText(getApplicationContext(), "Enter address in the search text box", Toast.LENGTH_LONG);
+//                toast.show();
+//            }
+//        }
+//    }
     // location permission region ends
 
 //    This method search the address entered by the user
-    private void search(List<Parking> parkings,String address)
+    private void search(List<Parking> parkings)
     {
+        EditText searchET=findViewById(R.id.search_et);
+        searchAddress=searchET.getText().toString();
         for(Parking parking: parkings){
-            if(address.toLowerCase().contentEquals(parking.toString().toLowerCase()))
+            if(parking.name.toLowerCase().contains(searchAddress.toLowerCase()))
             {
-                Toast toast = Toast.makeText(getApplicationContext(), "Address found", Toast.LENGTH_LONG);
-                toast.show();
+               Log.d("Location found",parking.name);
+                LatLng positionForCamera=new LatLng(Double.parseDouble(parking.getLatitude()),Double.parseDouble(parking.getLongitude()));
+                CameraPosition camPos = new CameraPosition(positionForCamera, 15, TILT_LEVEL, BEARING_LEVEL);
+                mMap.moveCamera(CameraUpdateFactory.newCameraPosition(camPos));
+                break;
             }
 
             else{
-                Toast toast = Toast.makeText(getApplicationContext(), "Address not found", Toast.LENGTH_LONG);
+                Toast toast = Toast.makeText(getApplicationContext(), "Could not able to locate", Toast.LENGTH_LONG);
                 toast.show();
             }
         }
 
+    }
 
+    // This method moves the camera based on the location found
+    public void moveCamera(GoogleMap mMap,LatLng position){
+
+//        CameraPosition camPos = new CameraPosition(position, ZOOM_LEVEL, TILT_LEVEL, BEARING_LEVEL);
+//        mMap.moveCamera(CameraUpdateFactory.newCameraPosition(camPos));
+    }
+
+    //This method styles the search button
+    public void styleSearchBtn(){
+        EditText searchET=findViewById(R.id.search_et);
+       searchET.addTextChangedListener(new TextWatcher() {
+           @Override
+           public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+               Drawable whiteCircle= ResourcesCompat.getDrawable(getApplicationContext().getResources(),R.drawable.circle_white_with_image,null);
+               searchBtn.setBackgroundDrawable(whiteCircle);
+               searchBtn.setTextColor(getResources().getColor(R.color.black,null));
+               searchBtn.setEnabled(false);
+               searchBtn.setVisibility(View.INVISIBLE);
+           }
+
+           @Override
+           public void onTextChanged(CharSequence s, int start, int before, int count) {
+               Drawable blackCircle= ResourcesCompat.getDrawable(getApplicationContext().getResources(),R.drawable.circle_black,null);
+               searchBtn.setBackgroundDrawable(blackCircle);
+               searchBtn.setTextColor(getResources().getColor(R.color.white,null));
+               searchBtn.setEnabled(true);
+               searchBtn.setVisibility(View.VISIBLE);
+           }
+
+           @Override
+           public void afterTextChanged(Editable s) {
+
+           }
+       });
     }
 
 
+
+    public void showToast(String message)
+    {
+        Toast.makeText(this,message,Toast.LENGTH_LONG).show();
+    }
+
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+            return;
+        }
+
+        if (PermissionUtils.isPermissionGranted(permissions, grantResults,
+                Manifest.permission.ACCESS_FINE_LOCATION)) {
+            // Enable the my location layer if the permission has been granted.
+            enableMyLocation();
+        } else {
+            // Display the missing permission error dialog when the fragments resume.
+            mPermissionDenied = true;
+        }
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+     shareLatitude=location.getLatitude();
+     shareLongitude=location.getLongitude();
+     LatLng positionforCamera=new LatLng(shareLatitude,shareLongitude);
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Log.d("Latitude","disable");
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Log.d("Latitude","enable");
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        Log.d("Latitude","status");
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
 }
