5056cd61f4ce61abb52046e65f70f84a0f0ab4a6
iamgu <iamgu@ACER>
2019-04-11 22:54:41 -0400

    0.1.8 Removed unwanted classes

---
+++ app/src/main/java/com/example/park_n_go/RegistrationActivity.java
@@ -0,0 +1,133 @@
+package com.example.park_n_go;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class RegistrationActivity extends AppCompatActivity {
+    // Field variables
+    private EditText emailTV, passwordTV,nameTV;
+    private Button regBtn;
+    private ProgressBar progressBar;
+    private RadioButton roleDriver;
+    private RadioButton roleHost;
+    private FirebaseAuth mAuth;
+    private static FirebaseDatabase database;
+    private static DatabaseReference databaseRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        mAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        databaseRef=database.getReference();
+
+        initializeUIComponents();
+
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerNewUser();
+            }
+        });
+    }
+
+    private void registerNewUser() {
+        progressBar.setVisibility(View.VISIBLE);
+
+        final String email, password,name;
+        email = emailTV.getText().toString();
+        password = passwordTV.getText().toString();
+        name=nameTV.getText().toString();
+
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(), "Please enter email...", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(), "Please enter password!", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "Registration successful!", Toast.LENGTH_LONG).show();
+                            progressBar.setVisibility(View.GONE);
+
+
+                             String role="";
+                             String userId=databaseRef.child("user").getKey();
+
+                            // Saving values in database
+                            if(roleDriver.isChecked()){
+                                role=roleDriver.getText().toString();
+
+                             }
+                            if(roleHost.isChecked()){
+                                role=roleHost.getText().toString();
+
+                            }
+
+                            databaseRef.child("users").child(userId).setValue(new User(name,role,email,password)).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Log.d("Writing data","Success");
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.d("Writing data","Failed");
+                                }
+                            });
+
+                            // Staring login activity
+                            Intent intent = new Intent(RegistrationActivity.this, LoginActivity.class);
+                            startActivity(intent);
+                        }
+                        else {
+                            Toast.makeText(getApplicationContext(), "Registration failed! Please try again later", Toast.LENGTH_LONG).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
+    }
+
+
+    private void initializeUIComponents() {
+        emailTV = findViewById(R.id.email);
+        passwordTV = findViewById(R.id.password);
+        regBtn = findViewById(R.id.register);
+        progressBar = findViewById(R.id.progressBar);
+        roleDriver=findViewById(R.id.driver);
+        roleHost=findViewById(R.id.host);
+        nameTV=findViewById(R.id.name);
+        roleDriver.setChecked(true);
+
+    }
+}
+
---
+++ app/src/main/java/com/example/park_n_go/User.java
@@ -0,0 +1,64 @@
+package com.example.park_n_go;
+
+public class User {
+    // fields
+    private String name;
+    private String role;
+    private String email;
+    private String password;
+
+    // Getter and setters
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    // Constructors
+    public User(){}
+
+    public User(String name, String role, String email, String password) {
+        this.name = name;
+        this.role = role;
+        this.email = email;
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", role='" + role + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
